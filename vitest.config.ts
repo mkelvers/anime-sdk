3d7558ea1506d1e48a3e0c6f3fb657144168eeee
mkelvers <mikkelelvers@outlook.com>
2026-09-17 17:56:54 +0200

    refactor: use tsconfig paths in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import { resolve } from 'node:path';
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@': resolve(import.meta.dirname, 'src'),
-    },
+    tsconfigPaths: true,
   },
   test: {
     environment: 'node',
